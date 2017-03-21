8bf55ad8987b6dc7433ac98128072ab25a7e3fdb
jisma <jisclara.babymathew7@gmail.com>
2017-03-21 15:53:14 +0530

    first commit

---
+++ file.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "currently logged user"$USER
+echo "no of current user"
+X=$who
+echo $X
+echo "your operating system"$OSTYPE
+
+
+
+
---
+++ file3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "olll info"
+uname -a
+echo " kernal name"
+uname -s
+echo "processor"
+uname -p
+echo "os"
+uname -o
+echo "kernal name"
+uname -r
+echo "version"
+uname -v
+echo "available shell"
+cat /etc/shells
+echo "path"
+$PATH
+echo "mouse"
+echo "cpu"
+cat /dev/input/mice
